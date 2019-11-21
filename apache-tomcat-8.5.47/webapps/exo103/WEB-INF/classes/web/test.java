c55be971a55def816f7656ab9ad9b8b1b45b7d84
bakkarim <mohamed.bakkari.etu@univ-lille.fr>
2019-11-21 20:51:35 +0100

    troixieme test OK OK

--- apache-tomcat-8.5.47/webapps/exo103/WEB-INF/classes/web/test.java
+++ apache-tomcat-8.5.47/webapps/exo103/WEB-INF/classes/web/test.java
@@ -2,33 +2,38 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Writer;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
-
-import javafx.stage.Window;
 
 
 @WebServlet("/bag")
 public class BagServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
-	Bag myBag = new Bag();
+
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res)
 	throws ServletException, IOException {
 
-		PrintWriter out = res.getWriter();
-		HttpSession session = req.getSession();
-		  if (session.isNew())
-	    	  session.setAttribute("myBag", myBag);
-	       else {
-	    	  myBag = (Bag)session.getAttribute("myBag");
-	    	  myBag.print(out);
-	      }
 
-		res.setContentType("text/html");
+				res.setContentType("text/html");
 
+				PrintWriter out = res.getWriter();
+		HttpSession session = req.getSession(true);
+		Bag myBag = new Bag();
+		if (session.isNew())
+				session.setAttribute("myBag", myBag);
+			 else {
+				myBag = (Bag)session.getAttribute("myBag");
+				myBag.print(out);
+			}
+
+		//session.setAttribute("bag",myBag);
+		//session.setAttribute("bag",myBag);
+	  //myBag =(session.getAttribute("bag")!=null) ? (Bag)session.getAttribute("bag") : myBag;
+	//	myBag.print(out);
 		out.append("<!DOCTYPE html>")
 	      .append("<html>")
 	      .append("<head>")
@@ -53,20 +58,35 @@ public class BagServlet extends HttpServlet {
 
 		String ref = req.getParameter("ref");
 		String qty = req.getParameter("qty");
+Bag myBag = new Bag();
+		res.setContentType("text/html");
+   //Bag myBag = new Bag();
+   HttpSession session = req.getSession();
+
 		PrintWriter out = res.getWriter();
 
+		/*out.append("<!DOCTYPE html>\r\n")
+				.append("<html>\r\n")
+				.append("		<head>\r\n")
+				.append("			<title>Welcome message</title>\r\n")
+				.append("		</head>\r\n")
+				.append("		<body>\r\n");*/
 
-		res.setContentType("text/html");
-
-
-		if (ref != null && !ref.trim().isEmpty() && qty != null && !qty.trim().isEmpty()) {
-		  	myBag.setItem(ref,Integer.parseInt(qty));
-		    res.sendRedirect("/exo103/bag");
+	  if (ref != null && !ref.trim().isEmpty() && qty != null && !qty.trim().isEmpty()) {
+			//out.append("	your ref is " + ref + ".\r\n");
+			//out.append("	your qty is " + qty + ".\r\n");
+			Integer qt=Integer.parseInt(qty);
+			//Bag bag = (Bag) session.getAttribute("bag");
+      myBag.setItem(ref,qt);
+			session.setAttribute("myBag", myBag);
+      res.sendRedirect("bag.jsp");
 	}
 
 	  else{
 			res.setStatus(400);
 		}
+		/*out.append("		</body>\r\n")
+				.append("</html>\r\n");*/
 
 
 }
